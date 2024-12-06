3ac1c6f767fbd1797c30396560aa4a0ff42dc6e8
Wendirad Demelash <wendiradame@gmail.com>
2024-12-06 11:02:57 -0800

    (hack) Add dummy test to prevent pytest failures

---
+++ tests/dummy_test.py
@@ -0,0 +1,10 @@
+"""
+This is a dummy test to prevent pytest from failing when no tests are defined.
+"""
+
+
+def test_dummy():
+    """
+    A dummy test to prevent pytest from failing when no tests are defined.
+    """
+    assert True
