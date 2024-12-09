2fe217a6b1854b6a5878802342da1adec163816b
Wendirad Demelash <wendiradame@gmail.com>
2024-12-09 00:12:12 -0800

    (feat) Perform Time Series Analysis

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -7,6 +7,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 def report_null_columns(df):
@@ -67,15 +68,13 @@ def plot_statistical_summary(
         figsize=(figsize[0], figsize[1] * nrows),
         constrained_layout=True,
     )
-    axes = axes.flatten()  # Flatten axes to iterate easily
+    axes = axes.flatten()
 
     for idx, method in enumerate(aggregation_methods):
-        # Resample the data using the specified method
         getattr(
             dataframe.resample(resample_period, on=time_column), method
         )().plot(ax=axes[idx], legend=False)
 
-        # Set titles and labels
         axes[idx].set_title(f"{title} ({method.capitalize()})")
         axes[idx].set_xlabel(xlabel)
         axes[idx].set_ylabel(ylabel)
@@ -88,8 +87,7 @@ def plot_statistical_summary(
         *axes[idx].get_legend_handles_labels(), loc="upper center", ncol=5
     )
 
-    # Adjust layout
-    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Leave space for the legend
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
     plt.show()
 
 
@@ -120,16 +118,14 @@ def plot_histogram_with_percentiles(
     if percentiles is None:
         percentiles = [10, 25, 50, 75, 90]
 
-    nrows = math.ceil(
-        len(columns) / ncols
-    )  # Calculate the number of rows needed
+    nrows = math.ceil(len(columns) / ncols)
     _, axes = plt.subplots(
         nrows,
         ncols,
         figsize=(figsize[0], figsize[1] * nrows),
         constrained_layout=True,
     )
-    axes = axes.flatten()  # Flatten axes array to iterate over it easily
+    axes = axes.flatten()
 
     for i, column in enumerate(columns):
         values = data[column]
@@ -153,7 +149,6 @@ def plot_histogram_with_percentiles(
         axes[i].set_ylabel("Frequency")
         axes[i].legend()
 
-    # Hide unused subplots
     for j in range(len(columns), len(axes)):
         axes[j].axis("off")
 
@@ -173,7 +168,7 @@ def plot_outliers(dataframe, columns, ncols=2, figsize=(15, 5)):
     Returns:
         None
     """
-    nrows = math.ceil(len(columns) / ncols)  # Determine the number of rows
+    nrows = math.ceil(len(columns) / ncols)
     _, axes = plt.subplots(nrows, ncols, figsize=figsize)
 
     axes = axes.flatten()
@@ -181,8 +176,6 @@ def plot_outliers(dataframe, columns, ncols=2, figsize=(15, 5)):
     for idx, column in enumerate(columns):
         sns.boxplot(data=dataframe[column], ax=axes[idx])
         axes[idx].set_title(f"Box Plot of {column}")
-
-    # Hide any unused subplots
     for idx in range(len(columns), len(axes)):
         axes[idx].axis("off")
 
@@ -190,7 +183,7 @@ def plot_outliers(dataframe, columns, ncols=2, figsize=(15, 5)):
     plt.show()
 
 
-def plot_entries(data, columns):
+def plot_entries(data, columns, figsize=(12, 8)):
     """
     Plot stacked bar charts showing the percentage distribution of negative,
     zero, and positive values for multiple columns.
@@ -222,7 +215,7 @@ def plot_entries(data, columns):
     bar_width = 0.6
     x = np.arange(len(columns))
 
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=figsize)
     plt.bar(
         x,
         negative_percent,
@@ -256,4 +249,145 @@ def plot_entries(data, columns):
     plt.legend()
     plt.grid(axis="y", linestyle="--", alpha=0.7)
     plt.tight_layout()
+    plt.show()
+
+
+def plot_trends(
+    dataframe,
+    columns,
+    *,
+    period="Month",
+    ncols=2,
+    figsize=(20, 10),
+    title_prefix="Solar Farm",
+):
+    """
+    Plot bar charts for specified columns by month and year in a grid layout.
+
+    Args:
+        dataframe (pd.DataFrame): The dataset containing the solar data with
+                                  a DateTime index.
+        columns (list): List of column names to plot.
+        period (str): The period to plot by (default: "Month").
+        ncols (int): Number of columns in the grid (default: 2).
+        figsize (tuple): Figure size as (width, height) (default: (20, 10)).
+        title_prefix (str): Prefix for the plot titles (default:
+                                                        "Benin Solar Farm").
+
+    Returns:
+        None
+    """
+
+    nrows = -(-len(columns) // ncols)
+    plt.figure(figsize=figsize)
+
+    for i, var in enumerate(columns):
+        plt.subplot(nrows, ncols, i + 1)
+        sns.barplot(
+            data=dataframe,
+            x={
+                "m": dataframe.index.month,
+                "d": dataframe.index.day,
+                "h": dataframe.index.hour,
+            }[period[0].lower()],
+            y=var,
+            hue=dataframe.index.year,
+        )
+        plt.title(f"{title_prefix} - {var} by {period}")
+        plt.xlabel(period)
+        plt.ylabel(var)
+        plt.legend(title="Year", bbox_to_anchor=(1.05, 1), loc="upper left")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_seasonal_decomposition(
+    data, columns, period, ncols=2, figsize=(20, 5)
+):
+    """
+    Perform seasonal decomposition and plot results for multiple
+    columns in a grid layout.
+
+    Args:
+        data (pd.DataFrame): The dataset with a DateTime index.
+        columns (list): List of column names to decompose.
+        period (int): The period of the seasonality
+                      (e.g., 365 for daily data over a year).
+        ncols (int): Number of columns in the grid layout.
+        figsize (tuple): Figure size for the entire grid.
+
+    Returns:
+        None
+    """
+    nrows = math.ceil(len(columns) / ncols)
+    _, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    axes = axes.flatten()
+
+    for i, column in enumerate(columns):
+
+        decomposition = seasonal_decompose(
+            data[column], model="additive", period=period
+        )
+
+        axes[i].plot(decomposition.trend, label="Trend", color="blue")
+        axes[i].plot(decomposition.seasonal, label="Seasonal", color="green")
+        axes[i].plot(decomposition.resid, label="Residual", color="red")
+        axes[i].set_title(f"Seasonal Decomposition of {column}")
+        axes[i].legend()
+
+    for i in range(len(columns), len(axes)):
+        axes[i].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_cleaning_impact(
+    data, columns, time_column=None, figsize=(15, 10), ncols=2
+):
+    """
+    Plot box plots and line plots for sensor readings before and after
+    cleaning in a grid.
+
+    Args:
+        data (pd.DataFrame): The dataset containing the cleaning,
+                             sensor, and time data.
+        columns (list): List of sensor columns to plot.
+        time_column (str): The name of the time column.
+        figsize (tuple): The size of the figure (default: (15, 10)).
+        ncols (int): Number of columns in the grid layout (default: 2).
+
+    Returns:
+        None
+    """
+    nrows = len(columns)
+    _, axes = plt.subplots(
+        nrows=nrows, ncols=ncols, figsize=figsize, constrained_layout=True
+    )
+
+    for i, column in enumerate(columns):
+        sns.boxplot(ax=axes[i, 0], data=data, x="Cleaning", y=column)
+        axes[i, 0].set_title(f"Efect: {column}")
+        axes[i, 0].set_xlabel("Cleaning (0 = Before, 1 = After)")
+        axes[i, 0].set_ylabel("Readings")
+
+        for status in data["Cleaning"].unique():
+            subset = data[data["Cleaning"] == status]
+            axes[i, 1].plot(
+                (
+                    subset[time_column]
+                    if time_column is not None
+                    else subset.index
+                ),
+                subset[column],
+                label=f"Cleaning {status}",
+            )
+        axes[i, 1].set_title(f"Trend Plot: {column}")
+        axes[i, 1].set_xlabel("Time")
+        axes[i, 1].set_ylabel("Readings")
+        axes[i, 1].legend(title="Cleaning Status")
+        axes[i, 1].grid()
+
+    plt.suptitle("Impact of Cleaning on Sensor Readings", fontsize=16)
     plt.show()
